da54f956be0d35257180aa323e48473a63853da5
Rana Awais <ranaawaix157@gmail.com>
2023-10-26 01:12:59 -0700

    statics added

--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.views.generic import CreateView
+
 
 # Create your views here.
